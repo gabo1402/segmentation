3cce22f792627664680c2fcba0bf94fa7a95ee7b
Gabriel Iñaki Lopez Monge <A01737727@tec.mx>
2025-04-09 00:51:54 -0600

    routes a las paginas de Estudiantes y Socios

---
+++ front/src/paginasE/VisualizarProyectosE.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavCub from '../componentes/navegacionE';
+
+function VisualizarProyectos() {
+    return (
+        <div className="cube">
+          <NavCub />
+          <h1>A que postularme</h1>
+        </div>
+      );
+    }
+
+export default VisualizarProyectos;
---
+++ front/src/paginasS/PostularProyectoS.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavCub from '../componentes/navegacionS';
+
+function PostularProyecto() {
+    return (
+        <div className="cube">
+          <NavCub />
+          <h1>Postular proyecto</h1>
+        </div>
+    );
+}
+
+export default PostularProyecto;
